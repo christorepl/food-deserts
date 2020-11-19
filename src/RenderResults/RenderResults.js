4a3d5d0ce6926a950cf39e4072ac07d9c0478b68
Christopher <chrisobrien416@gmail.com>
2020-11-18 21:08:57 -0500

    trying to render

--- src/RenderResults/RenderResults.js
+++ src/RenderResults/RenderResults.js
@@ -6,29 +6,58 @@ export default class RenderResults extends React.Component {
     static contextType = AppContext
 
     render() {
-        // console.log(this.context.stateResults)
+        console.log(this.context.stateResults)
         const { stateResults } = this.context
 
         // console.log(p, state[p])
 
         // const stateResult = stateResults.forEach(state => {
-        //     for (let p in state) {
-        //         return (
-        //             <>
-        //             {state}
-        //             </>
-        //         )}
-        // })
+        //     for (let a in state) { console.log(state[a].stateId)
+        //     // return(
+        //     //     <div className="stateContainer">
+        //     //         {state[a].stateName}
+        //     //     </div>
+        //     // )
+        // }})
 
-        let stateResult = stateResults.forEach(state => {
-            for(let [key, value] of Object.entries(state))
+        let jsxResults = stateResults.map(state => {
             return (
                 <div className="stateContainer">
-                    {key} : {value}
+                    <h1>{state[0].stateName}</h1>
+                    <div className="raceContainer">
+                        <h3>Racial Data:</h3>
+                        <li key={state[0].stateId}>Black: {state[0].black}%</li>
+                        <li key="fdsf"{...state[0].stateId}>White: {state[0].white}%</li>
+                        <li>Asian: {state[0].asian}%</li>
+                        <li>Hispanic: {state[0].hispanic}%</li>
+                    </div>
+                    <div className="electionContainer">
+                        <h3>Election Results Data:</h3>
+                        <li key={state[0].Trump}>Donald Trump: {state[0].Trump}%</li>
+                        <li key={state[0].Biden}>Joe Biden: {state[0].Biden}%</li>
+                    </div>
+                    <div className="povertyContainer">
+                        <h3>Poverty Data:</h3>
+                        <li key={state[0].povertyRate}>Poverty Rate: {state[0].povertyRate}%</li>
+                    </div>
+                    <div className="foodInsecurityContainer">
+                        <h3>Food Insecurity Data:</h3>
+                        <li key={state[0].foodInsecurity}>Household Food Insecurity Rate: {state[0].foodInsecurity}%</li>
+                    </div>
                 </div>
             )
         })
-        console.log(stateResult)
+        console.log(jsxResults)
+
+        // let stateResult = stateResults.forEach(state => {
+        //     for(let [key, value] of Object.entries(state))
+        //     return (
+        //         <div className="stateContainer">
+        //             {key} : {value}
+        //         </div>
+        //     )
+        // })
+
 
 
     //     const stateResult = stateResults.map(stateResults => {
@@ -41,7 +70,7 @@ export default class RenderResults extends React.Component {
        return (
            <>
            hi
-           {stateResult}
+           {jsxResults}
            </>
        )
     }
