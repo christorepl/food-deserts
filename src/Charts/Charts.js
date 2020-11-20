87b1d9be5f8679cab6d0779cd058961bfecf9e6e
Christopher <chrisobrien416@gmail.com>
2020-11-20 14:34:49 -0500

    done

--- src/Charts/Charts.js
+++ src/Charts/Charts.js
@@ -8,25 +8,6 @@ export default class Charts extends React.Component {
     render() {
         const labels = this.context.stateResults.map(state => state.abbrev)
         const electionResults = this.context.stateResults.map(state => state.electionData)
-        console.log(electionResults)
-
-        // const election2020 = Object.values(electionResults)
-        console.log(Object.values(electionResults))
-
-        const obj = { foo: 'bar', baz: 42 };
-        console.log('obj', Object.values(obj))
-       
-
-        // console.log('2020 results: ', electionResults)
-        // for (const [key, value] of Object.entries(electionResults).forEach) {
-        //     console.log(value.val)
-        // }
-
-        // const results = Object.entries(electionResults).forEach(([key, value]) => {
-        //     console.log(value)
-        // })
-        // console.log(turd)
-        // console.log(electionResult)
         const trump = this.context.stateResults.map(state => state.electionData.Trump)
         const biden = this.context.stateResults.map(state => state.electionData.Biden)
         const black = this.context.stateResults.map(state => state.raceData.black)
@@ -35,18 +16,9 @@ export default class Charts extends React.Component {
         const asian = this.context.stateResults.map(state => state.raceData.asian)
         const foodInsecurity = this.context.stateResults.map(state => state.foodInsecurity)
         const poverty = this.context.stateResults.map(state => state.povertyRate)
-
-        const twentyChartData = {
-            labels: ['red', 'blue'],
-            datasets: [
-                {
-                    label: 'lets see',
-                    data: [3, 5],
-                    backgroundColor:['red', 'blue']
-                }
-            ]
-        }
-
+        
+        const newElection = electionResults.map(state => Object.values(state))
+        console.log(newElection)
 
 
         const foodChartData = {
@@ -171,12 +143,6 @@ export default class Charts extends React.Component {
         return(
             <div className="chart">
                 <div className="electionChart">
-                <Pie 
-                    data={twentyChartData}
-                    width={1}
-                    height={1}
-                    options={{ maintainAspectRatio: true }}
-                />
                 <Bar
                     data={electionChartData}
                     width={100}
