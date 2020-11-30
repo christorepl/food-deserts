efd36eeac1b009792423cd0adc0f50cef06b6a34
Christopher <chrisobrien416@gmail.com>
2020-11-29 23:29:19 -0500

    tweaking

--- src/StatePage/StatePage.js
+++ src/StatePage/StatePage.js
@@ -1,15 +1,35 @@
 import React from 'react'
+import { Redirect } from 'react-router-dom'
+import { toast } from 'react-toastify'
 import AppContext from '../Context/AppContext'
+
+toast.configure()
 
 export default class StatePage extends React.Component {
     static contextType = AppContext
+    
+    redirectMessage = () => {
+        console.log('jerks')
+        toast.info('That page does not exist. Redirected to states index page.', {autoClose: 2000, hideProgressBar: true, position: "bottom-left", pauseOnHover: false, pauseOnFocusLoss: false})
+    }
+
     render() {
         const state = this.context.allStates.find(s => parseInt(s.fips) === parseInt(this.props.match.params.fips))
         console.log(state)
         return (
             <>
-            //conditional rendering here. if state is NOT found, toastify an alert and redirect them to the state index page
-            hey
+            {state
+            ?
+            <div className="statePage">
+                state
+            </div>
+            :
+            <>
+            {console.log('idiot')}
+            {this.redirectMessage()}
+            {/* <Redirect to="/states"/> */}
+            </>
+            }
             </>
         )
     }
