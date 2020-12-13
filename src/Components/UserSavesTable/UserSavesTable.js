7c361036e230baa538b303faa1f59d6e3095c07f
Christopher <chrisobrien416@gmail.com>
2020-12-12 20:12:43 -0500

    lots of fixes

--- src/Components/UserSavesTable/UserSavesTable.js
+++ src/Components/UserSavesTable/UserSavesTable.js
@@ -3,27 +3,27 @@ import { Link } from 'react-router-dom'
 import { Table, Thead, Tbody, Tr, Th, Td } from 'react-super-responsive-table'
 import AppContext from '../../Context/AppContext'
 
-export default class TableExample extends React.Component {
+export default class UserSavesTable extends React.Component {
     static contextType = AppContext
     render() {
     return (
       <Table>
         <Thead>
           <Tr>
-            <Th>Name</Th>
-            <Th>States</Th>
-            <Th>Date Modified</Th>
+            <Th key={'saveName'}>Name</Th>
+            <Th key={'states'}>States</Th>
+            <Th key={'modified'}>Date Modified</Th>
           </Tr>
         </Thead>
         <Tbody>
-            {this.context.userSaves.map(save => {
+            {this.context.userSaves.map((save, index) => {
             const formatDate = (dateString) => {
             const options = { year: "numeric", month: "long", day: "numeric" }
             return new Date(dateString).toLocaleDateString(undefined, options)
             }
             return (
             
-                <Tr>
+                <Tr key={index}>
                 <Td><Link to={`/saved-search/${save.save_name}`}>{save.save_name}</Link></Td>
                 <Td>
                 {save.state_names.replaceAll('"', '').replaceAll('[', '').replaceAll(']', '').replaceAll(',', ', ')}
