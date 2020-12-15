0c43d5dd9363e2d1f50e095a135725e363c1e4db
Christopher <chrisobrien416@gmail.com>
2020-12-15 18:14:59 -0500

    toast

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css';
 import './index.css';
+import 'react-toastify/dist/ReactToastify.css';
 import App from './App';
 
 ReactDOM.render(
